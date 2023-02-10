86b08c31a4c797eb7c2002d48b013528790c3c40
B4rn4rd0 <103604854+B4rn4rd0@users.noreply.github.com>
2023-02-10 16:15:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+objects = [];
+status = "";
+
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function modelLoaded() {
+    console.log("Modelo Carregado!");
+    status = true;
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+    objectName = document.getElementById("objectName").value;
+}
